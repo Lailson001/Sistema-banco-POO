ec9df45c771e6fb2411b078676ab3ad06b674243
Lailson Santos <147454335+Lailson001@users.noreply.github.com>
2025-03-10 16:20:40 +0000

    a1

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from _typeshed import IdentityFunction
-from abc import ABC, abstractclassmethod, abstractmethod
+
 from datetime import datetime
 
 
@@ -57,14 +56,79 @@ class Conta:
       return self._historico
       
     def sacar(self, valor):
-      if self.saldo >= valor:
-          self.saldo -= valor  # Atualiza o saldo corretamente
-          return True  # Confirma o saque
-      else:
-          print("Saldo insuficiente!")  # Mensagem de erro
-          return False  # Indica que o saque não foi realizado
+        saldo = self.saldo
+        excedeu_saldo = valor > saldo
 
-g
+        if excedeu_saldo:
+          print(f"Saldo insuficiente. Saldo disponível: {saldo}")
+
+        elif valor > 0:
+          self.saldo -= valor
+          print(f"Saque de {valor} realizado com sucesso. Saldo restante: {self.saldo}")
+          return True
+
+        else:
+          print("Valor inválido para saque.")
+          return False
+
+    def depositar(self, valor):
+        if valor > 0:
+          self.saldo += valor
+          print(f"Depósito de {valor} realizado com sucesso. Saldo atual: {self.saldo}")
+          return True
+
+        else:
+          print("Valor inválido para depósito.")
+          return False
+
+
+  class ContaCorrente(Conta):
+    def __init__(self, numero, cliente, limite=500, limite_saque=3):
+        super().__init__(numero, cliente)
+        self._limite = limite
+        self._limite_saque = limite_saque
+
+    def sacar(self, valor):
+        numero_saques = len(
+            [transacao for transacao in self._transacoes if transacao.tipo == "saque"]
+        )
+
+        excedeu_limite = numero_saques >= self._limite_saque
+
+        if excedeu_limite:
+            print("Limite de saques excedido.")
+            return False
+
+        else:
+          return super().sacar(valor)
+
+
+def __str__ (self):
+  return f"Conta {self._numero} - Saldo: {self._saldo}"
+
+class
+
+      
+       
+       
+       
+       
+         
+    
+
+         
+       
+
+
+
+
+
+      
+     
+
+           
+
+
      
 
 
